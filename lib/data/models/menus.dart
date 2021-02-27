a0afacb2b3c8f4e1837baf4463169f148553b2a1
Taufik Amaryansyah <taufik.amary@gmail.com>
2021-02-27 14:20:52 +0700

    change-model

--- lib/data/models/menus.dart
+++ lib/data/models/menus.dart
@@ -11,7 +11,7 @@ class Menus {
   });
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
+    final Map<String, dynamic> data = Map<String, dynamic>();
     if (this.foods != null) {
       data['foods'] = this.foods.map((v) => v.toJson()).toList();
     }
